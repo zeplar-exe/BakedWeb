a483279234ad30dc23284814522b4e2eed05822d
Zander Baker <zanderbaker100@gmail.com>
2022-07-15 15:57:59 -0700

    Create CanvasPainter, AlphaColor
    [File/Class Addition]: Create CanvasPainter
    [File/Class Addition]: Create AlphaColor
    
    [Project Modification]: Enable unsafe code in BakedWeb.csproj

---
+++ BakedWeb/CanvasPainter.cs
@@ -0,0 +1,20 @@
+﻿namespace BakedWeb;
+
+public class CanvasPainter
+{
+    public unsafe void Draw(ref byte[] target)
+    {
+        fixed (byte* b = target)
+        {
+            var colorCount = target.Length / AlphaColor.Size;
+            var redColor = new AlphaColor(255, 0, 0, 255);
+
+            for (var i = 0; i < colorCount; i++)
+            {
+                redColor.Write(b);
+
+                *b += AlphaColor.Size;
+            }
+        }
+    }
+}
